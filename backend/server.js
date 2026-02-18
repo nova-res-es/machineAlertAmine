ffb2a9f45ad2f6d31bb154a33aaa344a9eb49823
aminebenfraj <amed13515@gmail.com>
2026-02-18 18:03:02 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "https://machine-alert-frontend.onrender.com",
+      "https://novares-machine-alert.vercel.app",
       "http://localhost:3000",
       "http://localhost:5173",
     ],
